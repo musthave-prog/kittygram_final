a0f46d67ce70080f7de50cbc5adfbba5a37c437f
musthave-prog <darya.sim.ds@gmail.com>
2025-06-23 13:57:52 +0300

    ports

--- backend/kittygram_backend/urls.py
+++ backend/kittygram_backend/urls.py
@@ -9,18 +9,11 @@ router = routers.DefaultRouter()
 router.register(r'cats', CatViewSet)
 router.register(r'achievements', AchievementViewSet)
 
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('api/', include(router.urls)),
-#     path('api/', include('djoser.urls')),  # Работа с пользователями
-#     path('api/', include('djoser.urls.authtoken')),  # Работа с токенами
-# ]
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),  # REST API endpoints
-    # Djoser endpoints
-    path('api/auth/', include('djoser.urls')),  # Работа с пользователями
-    path('api/auth/token/', include('djoser.urls.authtoken')),  # токены
+    path('api/', include(router.urls)),
+    path('api/', include('djoser.urls')),  # Работа с пользователями
+    path('api/', include('djoser.urls.authtoken')),  # Работа с токенами
 ]
 
 if settings.DEBUG:
