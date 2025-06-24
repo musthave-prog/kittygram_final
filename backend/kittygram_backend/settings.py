703fb51a75cb8cd00f6fe58fc6d09cacf5084f5c
musthave-prog <darya.sim.ds@gmail.com>
2025-06-25 00:03:33 +0300

    change_6

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,14 +1,13 @@
 # flake8: noqa
+from dotenv import load_dotenv
 import os
 from pathlib import Path
-
-from dotenv import load_dotenv
 
 load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 DEBUG = os.getenv('DEBUG_MODE', 'False').lower() in ('true', '1')
 
